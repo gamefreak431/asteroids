461a7cce91924ecbeb08768ec400b0024ac2166c
gamefreak431 <mcleish.john@gmail.com>
2025-11-19 17:07:25 +1100

    Added deltatime to game loop and limtited it to 60fps

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from logger import log_state
 
 def main():
     pygame.init()
+    Clock = pygame.time.Clock()
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
@@ -13,6 +15,8 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        Clock.tick(60)
+        print(dt)
         pygame.display.flip()
 
 

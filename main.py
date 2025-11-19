c0f3015c7acec1afc78a725cda9837d926989fe8
gamefreak431 <mcleish.john@gmail.com>
2025-11-19 17:26:51 +1100

    created player script, updated constants with player details, added player to main so now the sprite spawns on the screen

--- main.py
+++ main.py
@@ -1,22 +1,24 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT 
 from logger import log_state
+from player import Player
 
 def main():
     pygame.init()
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     while True:
-        log_state()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill("black")
+        log_state()
         Clock.tick(60)
-        print(dt)
+        screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
 

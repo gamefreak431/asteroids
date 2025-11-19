b4766333b53d9c120041c777cca2c7a4ae9a26cb
gamefreak431 <mcleish.john@gmail.com>
2025-11-19 18:05:34 +1100

    updated spelling of updatable from updateable as it was failing the test

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ def main():
     Clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    updateable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
-    Player.containers = (updateable, drawable)
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
@@ -22,7 +22,7 @@ def main():
         Clock.tick(60)
         dt = Clock.get_time() / 1000  # Delta time in seconds.
         screen.fill("black")
-        updateable.update(dt)
+        updatable.update(dt)
         for sprite in drawable:
             sprite.draw(screen)
         pygame.display.flip()
